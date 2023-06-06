8c22e0d75f799aa9206b76a86b9a2fc110fc5d36
anurag <anuragchachan97@gmail.com>
2023-06-06 22:49:36 +0530

    Nav & Side Bar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,29 +1,47 @@
-import React from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
 
-const NavigationBar = () => {
+function NavigationBar() {
+  const { logout } = useContext(AuthContext);
   return (
-    <Navbar style={{ backgroundColor: "#FF8C00" }} variant="dark" expand="lg">
-      <Navbar.Brand href="#home" style={{ color: "#FFF" }}>
-        My Website
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <NavDropdown
-            title="Profile"
-            id="basic-nav-dropdown"
-            style={{ color: "#FFF" }}
-            className="order-lg-last"
-          >
-            <NavDropdown.Item href="#profile">My Profile</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
+    <Navbar
+      expand="lg"
+      fixed="top"
+      style={{ backgroundColor: "#FF8C00", height: "8vh" }}
+    >
+      <Container>
+        <Navbar.Brand href="#home" style={{ color: "#FFF", fontWeight: 500 }}>
+          MyCity360
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <NavDropdown
+              title="Hi, Anurag"
+              id="basic-nav-dropdown"
+              style={{ color: "#FFF" }}
+            >
+              <NavDropdown.Item href="#" onClick={() => logout()}>
+                Logout
+              </NavDropdown.Item>
+              {/* <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item> */}
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
-};
+}
 
 export default NavigationBar;
