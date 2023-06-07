060de92c1df8675307e2791ecdc443d22e422a74
anurag <anuragchachan97@gmail.com>
2023-06-07 13:39:20 +0530

    'fix'

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,12 @@
-import React, { useContext } from "react";
-import { LocalStorage } from "../shared/lib";
-import { Button } from "react-bootstrap";
-import { AuthContext } from "../context/AuthContext";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 import NavigationBar from "./NavigationBar";
 import Sidebar from "./SideBar";
+
 export default function Home() {
-  const { logout } = useContext(AuthContext);
-  const Navigate = useNavigate();
-
-  const handleLogout = async () => {
-    await logout();
-    Navigate("/login");
-  };
-
   return (
     <div>
       <NavigationBar />
       <Sidebar />
-      {/* <Button onClick={handleLogout}>Logout</Button> */}
     </div>
   );
 }
